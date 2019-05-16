625718315700eac1814afd5362ad59a9b0860445
ZGW <lleaff@qq.com>
2019-05-16 21:49:48 +0800

    :sparkles: add syntax analysis

---
+++ compile/syntax_analysis.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+void program();                     //程序
+void sub_program();                 //分程序
+void declaration_statement_table(); //说明语句表
+void execute_statement_table();     //执行语句表
+void execute_statement();           //执行语句
+void read_statement();              //读语句
+void write_statement();             //写语句
+void assignment_statement();        //赋值语句
+void condition_statement();         //条件语句
+void declaration_statement();       //说明语句
+void conditional_expression();      //条件表达式
+void arithmetic_expression();       //算数表达式
+void variable_declaration();        //变量说明
+void function_declaration();        //函数说明
+void variable();                    //变量
+void identifier();                  //标识符
+void letter();                      //字母
+void number();                      //数字
+void arguments();                   //参数
+void function_body();               //函数体
+void unsigned_integer();            //无符号整数
+void factor();                      //因子
+void term();                        //项
+void relational_operator();         //关系运算符
+
+void sub_program()
+{
+  sub_program();
+}
+void program()
+{
+  sub_program();
+}
+
+int main()
+{
+  return 0;
+}
